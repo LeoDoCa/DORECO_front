7301559d5e2a4f253ff792c52dc62e87f492b9f0
AnettVera <20223tn034@utez.edu.mx>
2025-08-01 22:34:33 -0600

    DRC-15: Agrega validaciones de usuario y feedback visual en UI

--- src/DORECO_front/src/config/http-client/axios-client.js
+++ src/DORECO_front/src/config/http-client/axios-client.js
@@ -3,7 +3,7 @@ import Cookies from "js-cookie"
 import Swal from "sweetalert2"
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
